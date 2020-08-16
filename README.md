f1e37e3192bdac91721bb6b3275cd2cb4b8d2e71
wikihong <hjpChinash@163.com>
2020-08-16 11:10:38 +0800

    Date: 2020.08.16 Msg: 创建redux中的store

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ react-transtion-group
 ## 使用antd实现todolist组件
   1. 官网: https://ant.design/docs/react/getting-started-cn
   2. ` yarn add antd`
+
+## 创建redux中的store
+  1. Store 的创建
+  2. `yarn add redux`
+  3. 创建store
--- todoList/src/TodoList.js
+++ todoList/src/TodoList.js
@@ -1,25 +1,25 @@
 import React ,{Component} from 'react'
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
+import store from './store'
 
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-];
 class TodoList extends Component {
+  constructor (props) {
+    super(props)
+    this.state = store.getState()
+    console.log(this.state)
+  }
+
   render () {
     return (
       <div style={{ marginTop: '10px', marginRight: '10px'}}>
         <div>
-          <Input placeholder="TodoInfo" style={{ width: '300px', marginRight: '10px'}}/>
+          <Input value={this.state.inputValue} placeholder="TodoInfo" style={{ width: '300px', marginRight: '10px'}}/>
           <Button type="primary">提交</Button>
           <List
             style={{ width: '300px'}}
             bordered
-            dataSource={data}
+            dataSource={this.state.list}
             renderItem={item => (
               <List.Item>
                 {item}
