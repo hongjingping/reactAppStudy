533988e09441e464fd44f4f2fd3d12af7fb26667
wikihong <hjpChinash@163.com>
2020-09-06 10:50:05 +0800

    date: 2020.09.06 Msg: actionTypes的拆分

--- todoList/src/TodoList.js
+++ todoList/src/TodoList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
 import store from './store'
+import {CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM} from './store/actionTypes'
 
 class TodoList extends React.Component {
   constructor (props) {
@@ -43,7 +44,7 @@ class TodoList extends React.Component {
 
   handleInputChange (e) {
     const action = {
-      type: 'change_input_change',
+      type: CHANGE_INPUT_VALUE,
       value: e.target.value
     }
     store.dispatch(action)
@@ -57,14 +58,14 @@ class TodoList extends React.Component {
 
   handleButtonClick () {
     const action = {
-      type: 'add_todo_item'
+      type: ADD_TODO_ITEM
     }
     store.dispatch(action)
   }
 
   handleItemDelete (index) {
     const action = {
-      type: 'delete_todo_item',
+      type: DELETE_TODO_ITEM,
       index
     }
     store.dispatch(action)
--- todoList/src/store/reducer.js
+++ todoList/src/store/reducer.js
@@ -1,3 +1,6 @@
+
+import {CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM} from './actionTypes'
+
 // 笔记本里面的数据
 const defaultState = {
   inputValue: '123',
@@ -9,18 +12,18 @@ const defaultState = {
 // action 
 // reducer可以接收state，不可以修改state
 export default (state = defaultState, action) => {
-  if (action.type === 'change_input_change') {
+  if (action.type === CHANGE_INPUT_VALUE) {
     const newState = JSON.parse(JSON.stringify(state))
     newState.inputValue = action.value
     return newState
   }
-  if (action.type === 'add_todo_item') {
+  if (action.type === ADD_TODO_ITEM) {
     const newState = JSON.parse(JSON.stringify(state))
     newState.list.push(newState.inputValue)
     newState.inputValue = ''
     return newState;
   }
-  if (action.type === 'delete_todo_item') {
+  if (action.type === DELETE_TODO_ITEM) {
     const newState = JSON.parse(JSON.stringify(state))
     newState.list.splice(action.index, 1)
     return newState;
