5b1436f1d3c4d83889fc8edb55e93e22374b9955
wikihong <hjpChinash@163.com>
2020-09-13 17:10:56 +0800

    Date:2020/09/13 Msg: react中使用路由

--- jianshuApp/jianshu/src/App.js
+++ jianshuApp/jianshu/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Provider} from 'react-redux'
 import {Globalstyle} from './style.js'
+import { BrowserRouter, Route } from 'react-router-dom'
 import {IconGlobalstyle} from './statics/iconfont/iconfont.js'
 import Header from './common/header'
 import store from './store'
@@ -9,9 +10,17 @@ function App() {
   return (
     <div className="App">
       <Provider store = {store}>
-        <Globalstyle></Globalstyle>
-        <IconGlobalstyle/>
-        <Header/>
+        <div>
+          <Globalstyle></Globalstyle>
+          <IconGlobalstyle/>
+          <Header/>
+          <BrowserRouter>
+            <div>
+              <Route path='/' render={() => <div>Home</div>}></Route>
+              <Route path='/detail' render={() => <div>Detail</div>}></Route>
+            </div>
+          </BrowserRouter>
+        </div>
       </Provider>
     </div>
   );
